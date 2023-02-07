012a0b700795406474589a503dbb4732eb22caaa
RuslanKontrobaev <ruslan.kontrobaev@mail.ru>
2023-02-07 21:07:06 +0400

    Upload project

---
+++ Project/ConvexHull/FormMain.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace CourseProject
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.pictureBox_Drawing = new System.Windows.Forms.PictureBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.button_Clean = new System.Windows.Forms.Button();
+            this.button_BuildShell = new System.Windows.Forms.Button();
+            this.label_CountPoints = new System.Windows.Forms.Label();
+            this.textBox_Info = new System.Windows.Forms.TextBox();
+            this.label_info = new System.Windows.Forms.Label();
+            this.button_NextStep = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Drawing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_Drawing
+            // 
+            this.pictureBox_Drawing.BackColor = System.Drawing.Color.White;
+            this.pictureBox_Drawing.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox_Drawing.Location = new System.Drawing.Point(3, 4);
+            this.pictureBox_Drawing.Name = "pictureBox_Drawing";
+            this.pictureBox_Drawing.Size = new System.Drawing.Size(747, 573);
+            this.pictureBox_Drawing.TabIndex = 0;
+            this.pictureBox_Drawing.TabStop = false;
+            this.pictureBox_Drawing.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_Drawing_MouseDown);
+            this.pictureBox_Drawing.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_Drawing_MouseMove);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(756, 531);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(19, 15);
+            this.labelX.TabIndex = 1;
+            this.labelX.Text = "X:";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(756, 555);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(17, 15);
+            this.labelY.TabIndex = 2;
+            this.labelY.Text = "Y:";
+            // 
+            // button_Clean
+            // 
+            this.button_Clean.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_Clean.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_Clean.Location = new System.Drawing.Point(759, 123);
+            this.button_Clean.Name = "button_Clean";
+            this.button_Clean.Size = new System.Drawing.Size(177, 53);
+            this.button_Clean.TabIndex = 2;
+            this.button_Clean.Text = "Очистить холст";
+            this.button_Clean.UseVisualStyleBackColor = false;
+            this.button_Clean.Click += new System.EventHandler(this.button_Clean_Click);
+            // 
+            // button_BuildShell
+            // 
+            this.button_BuildShell.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_BuildShell.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_BuildShell.Location = new System.Drawing.Point(759, 4);
+            this.button_BuildShell.Name = "button_BuildShell";
+            this.button_BuildShell.Size = new System.Drawing.Size(177, 53);
+            this.button_BuildShell.TabIndex = 1;
+            this.button_BuildShell.Text = "Построить выпуклую оболочку";
+            this.button_BuildShell.UseVisualStyleBackColor = false;
+            this.button_BuildShell.Click += new System.EventHandler(this.button_BuildShell_Click);
+            // 
+            // label_CountPoints
+            // 
+            this.label_CountPoints.AutoSize = true;
+            this.label_CountPoints.Location = new System.Drawing.Point(756, 505);
+            this.label_CountPoints.Name = "label_CountPoints";
+            this.label_CountPoints.Size = new System.Drawing.Size(117, 15);
+            this.label_CountPoints.TabIndex = 5;
+            this.label_CountPoints.Text = "Количество точек: 0";
+            // 
+            // textBox_Info
+            // 
+            this.textBox_Info.Location = new System.Drawing.Point(759, 197);
+            this.textBox_Info.Multiline = true;
+            this.textBox_Info.Name = "textBox_Info";
+            this.textBox_Info.Size = new System.Drawing.Size(177, 305);
+            this.textBox_Info.TabIndex = 6;
+            this.textBox_Info.Text = resources.GetString("textBox_Info.Text");
+            // 
+            // label_info
+            // 
+            this.label_info.AutoSize = true;
+            this.label_info.Location = new System.Drawing.Point(756, 179);
+            this.label_info.Name = "label_info";
+            this.label_info.Size = new System.Drawing.Size(81, 15);
+            this.label_info.TabIndex = 7;
+            this.label_info.Text = "Информация";
+            // 
+            // button_NextStep
+            // 
+            this.button_NextStep.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_NextStep.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_NextStep.Location = new System.Drawing.Point(759, 63);
+            this.button_NextStep.Name = "button_NextStep";
+            this.button_NextStep.Size = new System.Drawing.Size(177, 53);
+            this.button_NextStep.TabIndex = 8;
+            this.button_NextStep.Text = "Начать алгоритм по шагам";
+            this.button_NextStep.UseVisualStyleBackColor = false;
+            this.button_NextStep.Click += new System.EventHandler(this.button_NextStep_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(943, 579);
+            this.Controls.Add(this.button_NextStep);
+            this.Controls.Add(this.label_info);
+            this.Controls.Add(this.textBox_Info);
+            this.Controls.Add(this.label_CountPoints);
+            this.Controls.Add(this.button_BuildShell);
+            this.Controls.Add(this.button_Clean);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.pictureBox_Drawing);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Обучающее приложение по геометрическому алгоритму Грэхема";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Drawing)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_Drawing;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Button button_Clean;
+        private System.Windows.Forms.Button button_BuildShell;
+        private System.Windows.Forms.Label label_CountPoints;
+        private System.Windows.Forms.TextBox textBox_Info;
+        private System.Windows.Forms.Label label_info;
+        private System.Windows.Forms.Button button_NextStep;
+    }
+}
+
---
+++ Project/ConvexHull/FormMain.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System;
+
+namespace CourseProject
+{
+    public partial class FormMain : Form
+    {
+        Graphics graphics;
+        List<Point> points = new List<Point>(); // Исходный набор точек.
+        List<Point> pointsCopy = new List<Point>(); // Копия исходного набора точек для отрисовки точек.
+        List<Point> stack = new List<Point>();
+        int iter = 2; // Необходимо для корректного обхода точек при пошаговой работе алгоритма.
+        bool isFirstStep = true; // Необходимо для корректного начала пошаговой работы алгоритма.
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            graphics = pictureBox_Drawing.CreateGraphics();
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; // Делает линии между точками плавными.
+        }
+
+        void pictureBox_Drawing_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Не отрисовывать и не добавлять в список повторяющиеся точки.
+            if (!points.Contains(new Point(e.X, e.Y)))
+            {
+                points.Add(new Point(e.X, e.Y));
+                graphics.FillEllipse(new SolidBrush(Color.Black), e.X, e.Y, 5, 5); // Отрисовать нажатую точку на форме.
+                label_CountPoints.Text = "Количество точек: " + points.Count().ToString();
+            }
+            if (points.Count() >= 3) button_BuildShell.Enabled = true;
+        }
+
+        void pictureBox_Drawing_MouseMove(object sender, MouseEventArgs e)
+        {
+            labelX.Text = "X: " + e.X;
+            labelY.Text = "Y: " + e.Y;
+        }
+
+        void SelectStep(int step)
+        {
+            switch (step)
+            {
+                case 1: // Начальный шаг.
+                    label_CountPoints.Text = "Количество точек: 0";
+                    button_NextStep.Text = "Начать алгоритм по шагам";
+                    textBox_Info.Text = "Для начала работы работы нарисуйте минимум три точки на панели рисования. Затем нажмите на кнопку 'Начать алгоритм по шагам' или 'Построить выпуклую оболочку'" + Environment.NewLine + "В случае необходимости перерисовать точки, нажмите на кнопку 'Очистить холст'";
+                    break;
+
+                case 2: // Работа алгоритма.
+                    textBox_Info.Text = "Нажимайте на кнопку 'Следующий шаг', чтобы увидеть следующий шаг работы алгоритма." + Environment.NewLine + "В случае необходимости перерисовать точки, нажмите на кнопку 'Очистить холст'";
+                    button_NextStep.Text = "Следующий шаг";
+                    break;
+
+                case 3: // Конец работы.
+                    textBox_Info.Text = "Работа алгоритма завершена. Построена выпуклая оболочка." + Environment.NewLine + "Нажмите на кнопку 'Очистить холст', чтобы заново рисовать точки";
+                    button_NextStep.Text = "Начать алгоритм по шагам";
+                    isFirstStep = true;
+                    break;
+            }
+        }
+
+        void button_Clean_Click(object sender, System.EventArgs e)
+        {
+            graphics.Clear(pictureBox_Drawing.BackColor);
+            points.Clear();
+            button_NextStep.Enabled = true;
+            SelectStep(1);
+        }
+
+        void button_BuildShell_Click(object sender, System.EventArgs e)
+        {
+            if (points.Count() < 3 || points.Count() > 1000)
+            {
+                MessageBox.Show("Для работы алгоритма требуется не менее 3-х и не более 1000 точек.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            graphics.Clear(pictureBox_Drawing.BackColor);
+            pointsCopy.Clear();
+            pointsCopy.AddRange(points);
+            DrawUserPoints();
+            DrawShell(Graham(points));
+        }
+
+        List<Point> Graham(List<Point> points)
+        {
+            // Отсортировать все точки по X. Если есть точки с одинаковыми X, сортируем и по Y.
+            points = points.OrderBy(p => p.X).ThenBy(p => p.Y).ToList();
+
+            // Сортировка точек по возрастанию правого поворота.
+            for (var i = 1; i < points.Count(); i++)
+            {
+                var j = i;
+                while (j > 1 && !IsRightTurn(points[0], points[j - 1], points[j]))
+                {
+                    var temp = points[j];
+                    points[j] = points[j - 1];
+                    points[j - 1] = temp;
+                    j--;
+                }
+            }
+
+            List<Point> stack = new List<Point>(points.Take(2));
+            for (var i = 2; i < points.Count(); i++)
+            {
+                stack.Add(points[i]);
+                while (stack.Count() >= 3 && IsRightTurn(stack[stack.Count() - 1], stack[stack.Count() - 2], stack[stack.Count() - 3]))
+                    stack.RemoveAt(stack.Count() - 2);
+            }
+            SelectStep(3);
+            return stack;
+        }
+
+        bool IsRightTurn(Point a, Point b, Point c) => (b.X - a.X) * (c.Y - a.Y) - (b.Y - a.Y) * (c.X - a.X) < 0;
+
+        void DrawUserPoints()
+        {
+            for (var i = 0; i < pointsCopy.Count(); i++)
+                graphics.FillEllipse(new SolidBrush(Color.Black), pointsCopy[i].X, pointsCopy[i].Y, 5, 5);
+        }
+
+        void DrawShell(List<Point> points)
+        {
+            for (var i = 0; i < points.Count() - 1; i++)
+                graphics.DrawLine(new Pen(Color.Black, 5), points[i], points[i + 1]);
+            graphics.DrawLine(new Pen(Color.Black, 5), points[0], points[points.Count() - 1]);
+        }
+
+        void FormMain_FormClosing(object sender, FormClosingEventArgs e) => e.Cancel = MessageBox.Show("Вы действительно хотите выйти из программы?", "Завершение программы", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No;
+
+        void button_NextStep_Click(object sender, EventArgs e)
+        {
+            if (isFirstStep) // Первый шаг алогритма
+            {
+                if (points.Count() < 3 || points.Count() > 1000)
+                {
+                    MessageBox.Show("Для работы алгоритма требуется не менее 3-х и не более 1000 точек.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                isFirstStep = false;
+                SelectStep(2);
+                graphics.Clear(pictureBox_Drawing.BackColor);
+                pointsCopy.Clear();
+                pointsCopy.AddRange(points);
+                DrawUserPoints();
+
+                // Отсортировать все точки по X. Если есть точки с одинаковыми X, сортируем и по Y.
+                points = points.OrderBy(p => p.X).ThenBy(p => p.Y).ToList();
+
+                // Сортировка точек по возрастанию правого поворота.
+                for (var i = 1; i < points.Count(); i++)
+                {
+                    var j = i;
+                    while (j > 1 && !IsRightTurn(points[0], points[j - 1], points[j]))
+                    {
+                        var temp = points[j];
+                        points[j] = points[j - 1];
+                        points[j - 1] = temp;
+                        j--;
+                    }
+                }
+
+                stack = new List<Point>(points.Take(2));
+
+                graphics.DrawLine(new Pen(Color.Red, 3), stack[0], stack[1]); // Нарисовать линию между первой и второй точкой.
+            }
+            else
+            {
+                if (iter < points.Count())
+                {
+                    stack.Add(points[iter++]);
+                    graphics.DrawLine(new Pen(Color.Red, 3), stack[stack.Count() - 1], stack[stack.Count() - 2]); // Нарисовать линию без исключённой точкой.
+                    while (stack.Count() >= 3 && IsRightTurn(stack[stack.Count() - 1], stack[stack.Count() - 2], stack[stack.Count() - 3]))
+                    {
+                        stack.RemoveAt(stack.Count() - 2);
+                        graphics.DrawLine(new Pen(Color.Blue, 3), stack[stack.Count() - 1], stack[stack.Count() - 2]); // Нарисовать линию с исключённой точкой.
+                    }
+                    SelectStep(2);
+                }
+                else if (iter == points.Count())
+                {
+                    iter++;
+                    graphics.DrawLine(new Pen(Color.Red, 3), stack[0], stack[stack.Count() - 1]); // Нарисовать линию между первой и последней точкой.
+                }
+                else
+                {
+                    iter = 2;
+                    DrawShell(stack);
+                    SelectStep(3);
+                    isFirstStep = true;
+                    button_NextStep.Enabled = false;
+                    button_BuildShell.Enabled = false;
+                }      
+            }
+        }
+    }
+}
